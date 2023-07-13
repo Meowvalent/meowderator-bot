ee7f24a80354b665545c6f1c6150ae5186793a3d
Mark Valentin <mavalentmv@gmail.com>
2023-07-12 23:55:11 -0400

    Added in a basic !catfacts command

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
+)
+
+const CatFactAPIURL = "https://catfact.ninja/fact?max_length=140"
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+type CatFact struct {
+	Fact string `json:"fact"`
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!catfact" {
+
+		//Call the KuteGo API and retrieve our cute Dr Who Gopher
+		response, err := http.Get(CatFactAPIURL)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode == 200 {
+			body, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			var data CatFact
+			err = json.Unmarshal(body, &data)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			_, err = s.ChannelMessageSend(m.ChannelID, data.Fact)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Error: Can't get dr-who Gopher! :-(")
+		}
+	}
+}
